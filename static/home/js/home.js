d04401ac1cefb50f2940592fbee2f5a28a6288a8
chenpengdezhanghao <1320005907@qq.com>
2018-12-06 03:55:43 -0800

    轮播图完成

---
+++ static/home/js/home.js
@@ -0,0 +1,14 @@
+$(function () {
+
+    console.log('aaaa')
+     var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween: 30,
+        loop: true,
+        autoplay:1000,
+    });
+})
